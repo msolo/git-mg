1dcd1f62045c868f9404a533b7eec1eee1d8da44
Mike Solomon <msolo@gmail.com>
2020-09-07 16:36:15 -0700

    Tighten up the API and make it more composable.

--- gitapi/bash.go
+++ gitapi/bash.go
@@ -5,7 +5,7 @@ import "strings"
 const safeUnquoted = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@%_-+=:,./"
 
 // Return a string quoted for use in bash. This prefers single-quoted outputs to disable unnecessary secondary evaluation. The main use is printing a debug string that can be safely copy-pasted into a shell for further debugging.
-func BashQuoteWord(s string) string {
+func bashQuoteWord(s string) string {
 	// Double escaping ~ neuters expansion and ~ is implicit.
 	if strings.HasPrefix(s, "~/") {
 		return s
@@ -26,10 +26,10 @@ func BashQuoteWord(s string) string {
 	return "'" + strings.Replace(s, "'", "'\"'\"'", -1) + "'"
 }
 
-func BashQuoteCmd(args []string) string {
+func BashQuote(args... string) []string {
 	out := make([]string, len(args))
 	for i, x := range args {
-		out[i] = BashQuoteWord(x)
+		out[i] = bashQuoteWord(x)
 	}
-	return strings.Join(out, " ")
+	return out
 }
